a373096d3674f513155a55c6d3e0b3ff0068c96e
mo2men221 <115307181+mo2men221@users.noreply.github.com>
2025-08-25 12:54:44 +0300

    Create problem: (N) Numbers Histogram.cpp

---
+++ ProblemSolving/Sheet #2(Loops)/problem: (N) Numbers Histogram.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+    char S;
+    int A,B;
+    cin >> S >> A ;
+    for (int i =0;i<A;i++) {
+        cin >> B;
+        for (int j =0; j < B; j++) {
+            cout << S;
+        }
+        cout << endl;
+    }
+    }
