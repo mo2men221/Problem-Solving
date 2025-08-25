cf0e4d2f19c901abf47faa1280c7548899f0176a
mo2men221 <115307181+mo2men221@users.noreply.github.com>
2025-08-25 13:04:24 +0300

    Create problem: (G) Palindrome Array.cpp

---
+++ ProblemSolving/Sheet #3 (Array)/problem: (G) Palindrome Array.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+    int n;
+    cin >> n;
+    int arr[n];
+    bool is_palindrome = true;
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+    for (int i = 0; i < n; i++) {
+        if (arr[i] != arr[n-(i+1)]) {
+            is_palindrome = false;
+        }
+    }
+    if (is_palindrome == true) {
+        cout << "YES";
+    }
+    else {
+        cout << "NO";
+    }
+    }
