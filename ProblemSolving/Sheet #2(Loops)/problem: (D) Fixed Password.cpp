8a81d99c937e9f2835bc59a030a8abc4ea792265
mo2men221 <115307181+mo2men221@users.noreply.github.com>
2025-08-25 12:41:37 +0300

    Create  problem: (D) Fixed Password.cpp

---
+++ ProblemSolving/Sheet #2(Loops)/problem: (D) Fixed Password.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+    int password;
+    while (password !=1999) {
+        cin >> password;
+        if (password != 1999) {
+            cout << "Wrong" << endl;
+        }
+        else {
+            cout << "Correct" << endl;
+            return 0;
+        }
+    }
+}
